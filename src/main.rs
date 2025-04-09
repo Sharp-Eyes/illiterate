7bb0a462c61ee2f96b6c0fe14e02649a45c91507
Chromosomologist <chromosomologist@gmail.com>
2025-04-09 16:34:53 +0200

    feat: use cmd-thing for commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{env, error::Error, sync::Arc};
+use anyhow::Result;
 use bpe_thing::{generate_gibberish, load_tokens, tokenize, tokens_to_string};
+use cmd_thing::{Command, Flag};
 use rand::seq::IndexedRandom;
+use std::{env, error::Error, sync::Arc};
 use twilight_cache_inmemory::{DefaultInMemoryCache, ResourceType};
 use twilight_gateway::{
     ConfigBuilder, Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt as _,
@@ -11,8 +13,13 @@ use twilight_model::{
         payload::outgoing::update_presence::UpdatePresencePayload,
         presence::{ActivityType, MinimalActivity, Status},
     },
-    id::Id,
+    id::{
+        marker::{ChannelMarker, MessageMarker},
+        Id,
+    },
 };
+
+const PREFIX: &'static str = "i!";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -64,53 +71,99 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn check_prefix(content: &str, prefix: &str) -> bool {
+    // Assumes content comes without leading whitespace.
+    content
+        .chars()
+        .zip(prefix.chars())
+        .all(|(c, p)| c.eq_ignore_ascii_case(&p))
+}
+
 async fn handle_event(
     event: Event,
     http: Arc<HttpClient>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match event {
-        Event::MessageCreate(msg)
-            // if msg.channel_id == Id::new(1128297878855626873)
-                if msg.author.id != Id::new(1169608948081492038)
-                    && msg.author.id != Id::new(1083860445275881493) =>
-        {
-            if msg.content.starts_with("i!harry") {
-                let token_grammar = load_tokens("./bpe/harry.bpe").map_err(|e| {
-                    log::error!("error loading bpe: {}", e);
-                    e
-                })?;
-
-                let seed = if let Some((_, input)) = msg.content.split_once(" ") {
-                    tokenize(input.to_string(), &token_grammar)
-                } else {
-                    let mut rng = rand::rng();
-                    vec![token_grammar.choose(&mut rng).unwrap().1.clone()]
-                };
-
-                let mut seed_token_iter = seed.into_iter();
-                let last = seed_token_iter.next_back();
- 
-                let content = format!(
-                    "{}{}",
-                    tokens_to_string(&seed_token_iter.collect(), &token_grammar)?,
-                    generate_gibberish(
-                        &last.unwrap(),
-                        &token_grammar,
-                        20,
-                        1.0,
-                        1.0
-                    )?
-                );
-
-                http.create_message(msg.channel_id)
-                    .reply(msg.id)
-                    .content(&content)
-                    .await?;
-
+        Event::MessageCreate(msg) => {
+            if msg.author.id == Id::new(1169608948081492038)
+                && msg.author.id == Id::new(1083860445275881493)
+                || !check_prefix(&msg.content, PREFIX)
+            {
+                return Ok(());
             }
-            
+
+            let mut command = Command::parse(msg.content[PREFIX.len()..].to_string())?;
+            let subcommand = command.get_next_argument()?;
+
+            match subcommand.as_str() {
+                "harry" | "h" => {
+                    handle_bpe_gen(command, "./bpe/harry.bpe", http, msg.channel_id, msg.id).await?
+                }
+                "arknights" | "ak" => {
+                    handle_bpe_gen(command, "./bpe/arknights.bpe", http, msg.channel_id, msg.id)
+                        .await?
+                }
+                _ => todo!(),
+            };
+
             Ok(())
         }
-        _ => {Ok(())}
+        _ => Ok(()),
     }
+}
+
+async fn handle_bpe_gen(
+    mut command: Command,
+    file: &str,
+    http: Arc<HttpClient>,
+    channel_id: Id<ChannelMarker>,
+    message_id: Id<MessageMarker>,
+) -> Result<()> {
+    let depth = Flag::new("depth")
+        .alias("d")
+        .default(20)
+        .parse(&mut command)?;
+    let freq_weight = Flag::new("freq-weight")
+        .alias("f")
+        .default(1.0)
+        .parse(&mut command)?;
+    let idx_weight = Flag::new("idx-weight")
+        .alias("i")
+        .default(1.0)
+        .parse(&mut command)?;
+
+    let token_grammar = load_tokens(file).map_err(|e| {
+        log::error!("error loading bpe: {}", e);
+        e
+    })?;
+
+    let seed_str = command.drain_arguments().make_contiguous().join(" ");
+    let seed = if seed_str.is_empty() {
+        let mut rng = rand::rng();
+        vec![token_grammar.choose(&mut rng).unwrap().1.clone()]
+    } else {
+        tokenize(seed_str, &token_grammar)
+    };
+
+    let mut seed_token_iter = seed.into_iter();
+    let last = seed_token_iter.next_back();
+
+    let content = format!(
+        "{}{}",
+        tokens_to_string(&seed_token_iter.collect(), &token_grammar)?,
+        generate_gibberish(
+            &last.unwrap(),
+            &token_grammar,
+            depth,
+            freq_weight,
+            idx_weight
+        )?
+    );
+
+    http.create_message(channel_id)
+        .reply(message_id)
+        .content(&content)
+        .await?;
+
+    Ok(())
 }
